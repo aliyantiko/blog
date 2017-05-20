af6d63e6f138af4030775b2707102d593dd9b20a
aliyantiko <aliyantiko@gmail.com>
2017-05-21 00:11:54 +0700

    halaman index

--- index.php
+++ index.php
@@ -3,17 +3,28 @@
     require_once "view/header.php";
 
     $articles = tampilkan();
+
+    if(isset($_GET['cari'])){
+      $cari     = $_GET['cari'];
+      $articles = hasil_cari($cari);
+    }
 ?>
+
+<form action="" method="get">
+  <input type="search" name="cari" placeholder="masukan kata kunci">
+</form>
+
 <?php while($row = mysqli_fetch_assoc($articles)):?>
   <div class="each_article">
-    <h3><?= $row['judul']; ?></h3>
+    <h3><a href="single.php?id=<?= $row['id']; ?>"><?= $row['judul']; ?></a></h3>
     <p>
-      <?= $row['isi']; ?>
+      <?= excerpt($row['isi']); ?>
     </p>
 
     <p class="waktu"> <?= $row['waktu']; ?> </p>
     <p class="tag"> Tag:<?= $row['tag']; ?> </p>
-
+    <a href="edit.php?id=<?= $row['id']; ?>"> Edit </a>
+    <a href="delete.php?id=<?= $row['id']; ?>"> Hapus </a>
   </div>
 <?php endwhile; ?>
 <?php
